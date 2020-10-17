be0d98b49a148773c703c36a545aa37a147b4a62
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2020-10-17 23:32:56 +0300

    mongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.json({msg: 'connected'});
@@ -11,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/test/',require('./routes/test'));
+
 
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
