b17be8c28d3abf8f9e06f463e155aaecc04c2867
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2020-10-22 17:33:06 +0300

    prettier

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 connectDB();
 
-app.get('/', (req, res) => {
-    res.json({msg: 'connected'});
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({ msg: "connected" });
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
-app.use('/api/auth', require('./routes/auth'));
-
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
