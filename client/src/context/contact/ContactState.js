b76e7a037c931ff802e3eee7042c15c0b83f02d0
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2020-10-31 00:31:33 +0300

    progress

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,83 @@
+import React, { useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  CLEAR_CURRENT,
+  CLEAR_FILTER,
+  DELETE_CONTACT,
+  FILTER_CONTACTS,
+  REMOVE_ALERT,
+  SET_ALERT,
+  SET_CURRENT,
+  UPDATE_CONTACT,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jull@gmail.com',
+        phone: '111-111-1111',
+        type: 'personal',
+      },
+      {
+        id: 2,
+        name: 'Sara Watson',
+        email: 'sara@gmail.com',
+        phone: '222-222-2222',
+        type: 'personal',
+      },
+      {
+        id: 3,
+        name: 'Hary Osborn',
+        email: 'greengoblin@gmail.com',
+        phone: '333-333-3333',
+        type: 'professional',
+      },
+    ],
+    current: null,
+  };
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
+
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
