ca44dfe4f6f6f3f704fd1d5de798799b6dbbbdda
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2020-11-01 22:44:05 +0300

    progress

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,38 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { check, validationResult } = require("express-validator");
-const User = require("../models/User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 
 // @route     POST api/users
 // @desc      Register a user
 // @access Public
 
 router.post(
-  "/",
+  '/',
   [
-    check("name", "Nam e is required").not().isEmpty(),
-    check("email", "Please include a valid email.").isEmail(),
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email.').isEmail(),
     check(
-      "password",
-      "Please enter a password with 6 or more characters"
+      'password',
+      'Please enter a password with 6 or more characters'
     ).isLength({ min: 6, max: 16 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
     const { name, email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
-      if (user) res.status(400).json({ msg: "User already exists." });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists.' });
+      }
 
       user = new User({ name, email, password });
 
@@ -45,7 +49,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get("jwtSecret"),
+        config.get('jwtSecret'),
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
@@ -54,7 +58,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Server error");
+      res.status(500).send('Server error');
     }
   }
 );
